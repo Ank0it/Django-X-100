03264eb353784281d16a0034d4cb418b341189fe
Ankit Patel <131870832+Ank0it@users.noreply.github.com>
2026-02-08 13:55:39 +0530

    Add media settings and initial migration for AppVariety model; update URL configuration

--- Django/app/models.py
+++ Django/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class AppVariety(models.Model):
+    APP_TYPE_CHOICES = [
+        ('AI','Artificial Intelligence'),
+        ('ML','Machine Learning'),
+        ('GA','Generative AI'),
+        ('AA','Agentic AI'),
+    ]
+
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='apps')
+    date_added=models.DateTimeField(default=timezone.now) 
+    type=models.CharField(max_length=2, choices=APP_TYPE_CHOICES)
